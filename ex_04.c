669a758ec9648d8ada18b60c5f188ba674758c2d
MatheusEvangelistaTF <matheusevangelistat@gmail.com>
2025-04-24 18:07:15 -0300

    Add files via upload

---
+++ ex_04.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/*Nesse programa se pede que se insira 2 anos aleatórios com o valor int, e a partir disso, se checa quais são os anos bissextos entre elas e mostra eles por meio do printf.
+ele faz um cálculo onde o ano é dividido e se checa o seu resto, a partir disso é possível descobrir se o ano é bissexto ou não. (O ano bissexto sempre ocorre a cada 4 anos.)
+*/
+int main(){
+    int ano1, ano2;
+
+    printf("Digite o primeiro ano (menor): ");
+    scanf("%d", &ano1);
+
+    printf("Digite o segundo ano (maior): ");
+    scanf("%d", &ano2);
+
+    if (ano1 >= ano2){
+        printf("Erro: o primeiro ano deve ser menor que o segundo.\n");
+        return 1;
+    }
+
+    printf("Anos bissextos entre %d e %d:\n", ano1, ano2);
+
+    for (int ano = ano1; ano <= ano2; ano++){
+        if ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0)){
+            printf("%d\n", ano);
+        }
+    }
+
+    return 0;
+}
